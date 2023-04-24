a3fec2aca16758ff3dbecdfa356bc382c64106fc
ntimba <hello@ntimba.com>
2023-04-24 16:06:33 +0200

    - Configuration and setup of Dockerfile and docker-compose.yml files. - Finalization of the README.md file. - Configuration and usage of the Sass preprocessor

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// Information de connexion à la base de donnnées
+define('DB_NAME', 'ntimbablog');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'userpassword');
+define('DB_HOST', 'mysql');
+
+// Adresse du site
+define('SITE_URL', 'http://localhost:8080/');
